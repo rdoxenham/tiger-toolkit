355fe0e024fbaed486653f30f00ec128d2da99cd
Rhys Oxenham <roxenham@redhat.com>
2013-12-30 02:17:48 +0000

    Wrote make-repos.py script

--- deploy.py
+++ deploy.py
@@ -2,19 +2,22 @@
 
 import sys
 import os
+import getopt
+import getpass
+import time
+import datetime
 
 VERSION = "1.0"
 
 def usage():
-	print "====================================="
 	print "OpenStack Deployment Tool Version %s" % VERSION
-	print "====================================="
 	print "Usage: " + sys.argv[0],
 	print "<option> [argument]\n"
 	print "\t-h, --help\t\tPrints this usage/help menu"
 	print "\t-i, --interactive\tRuns tool in interactive mode"
 	print "\t-b, --basic\t\tDeploys basic/single-controller OpenStack environment"
 	print "\t-a, --advanced\t\tDeploys highly-available multi-node OpenStack environment"
+	print "\t-p, --post\t\tRun post-install configuration script"
 	print "\n\tExamples: " + sys.argv[0],
 	print " --interactive"
 	print "\t\t  " + sys.argv[0],
